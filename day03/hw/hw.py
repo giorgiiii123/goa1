9ae0828251f00d15f1944c8af48b854edff094d5
giorgiiiii123 <amiridzegiorgi172@gmail.com>
2024-07-04 12:21:02 +0400

    commit

--- day03/hw/hw.py
+++ day03/hw/hw.py
@@ -27,19 +27,57 @@
 concatination --> შეერთება
 
 """
+
+"""
+შექმენი ცვლადი name, მიანჭე gio მნიშვნელობა 1 ხაზზე მე-2 ხაზზე daviti და დაპრინტე საბოლლოო მნიშვნელობა
+"""
+
+# 6 
+"""
+შექმენი 4 ცვლადი num1 num2 num3 num4 
+ცვალდების მნიშვნელობა უნდა იყოს მხოლოდ int ტიპის 2,23,4,5...
+5 მაგალითი დაწერე +, -, /, //, *
+
+"""
+# 7 
+"""
+ამიხსენი რას აკეთებს ცვლადი 
+და დამიპირნტე 
+int, str რა განმსხვავებაა ამ ორს შორის
+"""
+
+# 8 
+"""
+შექმენი ცვლადი name----- str ტიპის
+შექმენი ცვლადი age----- int ტიპის
+
+შეაერთე ეს ცვლადები და დამიპრინტე (მათ შორის იყოს დაშორება)
+"""
+
+# 9
+"""
+შექმენი ცვლადი და ჩაწერე შენი ემოცია როგორ გაწვალებს დავითი!
+"""
+
+# 10
+"""
+შექმენი ცვლადი და შეანახიე სხვა ცვლადი
+"""
+
+
 #dAVALEBA1
 
 name1="gio"
 name2="tamuna"
 name3="nuki"
 name4="aleko"
-print(name1+name2+name3+name4)
+print(name1 +" "+name2+" "+name3+" "+name4)
 # DAVALEBA2
 age=13
-age=19
-age=50
-age=40
-print(age+10+age+10)
+nuki=19
+aleko=50
+tamuna=40
+print(age + 10 + nuki + 10+ aleko + 10 + tamuna + 10)
 # davaleba3
 future_me=13
 future_me=13+10
@@ -48,29 +86,36 @@ future_me=13+10
 about="ramdeni wkali dalia daviTma ak ro iko"
 amou_of_wather = 5
 
+# davaleba5
+
+name="gio"
+football="davit"
+print(name + " " + football)
+
+# davaleba6
+num1 = 23 + 21
+num2 = 15 - 6
+num3 = 15 / 3
+num4 = 20 // 2
+num5 = 10 * 2
+print(num1,num2,num3,num4,num5)
+# davaleba7
+cvladi = "cvladi inaxavs informacias,"
+print(cvladi)
+
+# davaleba8?
 
 
 
+# davaleba9
+
+wvaleba= "daviti mawvalebs iserogorc patara bavsebs atxovnineben xolme kanpets igi aris daundobeli"
+print(wvaleba)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# football"kalatburti"
+# kalatburti"footbaall"
 
 
 
